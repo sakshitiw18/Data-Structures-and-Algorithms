796eec5e78b776798ca84402ba5962b8e94a016c
sakshi <sakshitiw05@gmail.com>
2026-09-22 21:07:17 +0530

    Time: 298 ms (57.57%) | Memory: 80.1 MB (9.06%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -2,16 +2,15 @@ class Solution {
     public int countPrimes(int n) {
         if (n <= 2) return 0;
 
-        boolean[] notPrime = new boolean[n];
-        int count = 0;
+        boolean[] composite = new boolean[n];
+        int count = n / 2;
 
-        for (int i = 2; i < n; i++) {
-            if (!notPrime[i]) {
-                count++;
-
-                if ((long) i * i < n) {
-                    for (int j = i * i; j < n; j += i) {
-                        notPrime[j] = true;
+        for (int i = 3; i * i < n; i += 2) {
+            if (!composite[i]) {
+                for (int j = i * i; j < n; j += 2 * i) {
+                    if (!composite[j]) {
+                        composite[j] = true;
+                        count--;
                     }
                 }
             }
